33abd6c20fc75cba409c8a409f4f8a5b0e4bbae0
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2021-03-28 20:24:59 +0200

    add texture for door

--- src/script.js
+++ src/script.js
@@ -16,6 +16,40 @@ const size = {
 // setup data gui
 const gui = new dat.GUI();
 
+// get textures
+
+const loadingManager = new THREE.LoadingManager();
+
+loadingManager.onStart = () => {
+    console.log('start loading');
+};
+
+loadingManager.onLoad = () => {
+    console.log('loaded');
+};
+
+loadingManager.onProgress = () => {
+    console.log('progress loading');
+};
+
+const texture = new THREE.TextureLoader(loadingManager);
+const ambientTexture = texture.load(
+    '/textures/Concrete_Blocks_007_ambientOcclusion.jpg',
+);
+const baseTexture = texture.load('/textures/Concrete_Blocks_007_basecolor.jpg');
+const heightTexture = texture.load('/textures/Concrete_Blocks_007_height.jpg');
+const normalTexture = texture.load('/textures/Concrete_Blocks_007_normal.jpg');
+const roughnessTexture = texture.load(
+    '/textures/Concrete_Blocks_007_roughness.jpg',
+);
+const doorAmbientTexture = texture.load('/textures/door/ambientOcclusion.jpg');
+const doorBaseTexture = texture.load('/textures/door/color.jpg');
+const doorNormalTexture = texture.load('/textures/door/normal.jpg');
+const doorRoughnessTexture = texture.load('/textures/door/roughness.jpg');
+const doorHeightTexture = texture.load('/textures/door/height.jpg');
+const doorMentalnessTexture = texture.load('/textures/door/metalness.jpg');
+const doorAlphaTexture = texture.load('/textures/door/alpha.jpg');
+
 // create flat ground under scene
 const geometryPlane = new THREE.PlaneGeometry(20, 20);
 const materialPlane = new THREE.MeshBasicMaterial({
@@ -32,20 +66,62 @@ gui.add(ground.rotation, 'x').min(groundRotX).max(2).step(0.001);
 
 // create pyramid
 const geometryPyramid = new THREE.ConeGeometry(5, 5, 3);
-const materialPyramid = new THREE.MeshBasicMaterial({ color: '#1078bd' });
+const materialPyramid = new THREE.MeshBasicMaterial({
+    map: baseTexture,
+    aoMap: ambientTexture,
+    transparent: true,
+    displacementMap: heightTexture,
+    normalMap: normalTexture,
+    roughnessMap: roughnessTexture,
+});
+
+baseTexture.repeat.set(32, 8);
+ambientTexture.repeat.set(32, 8);
+heightTexture.repeat.set(32, 8);
+normalTexture.repeat.set(32, 8);
+roughnessTexture.repeat.set(32, 8);
+
+baseTexture.wrapS = THREE.RepeatWrapping;
+ambientTexture.wrapS = THREE.RepeatWrapping;
+heightTexture.wrapS = THREE.RepeatWrapping;
+normalTexture.wrapS = THREE.RepeatWrapping;
+roughnessTexture.wrapS = THREE.RepeatWrapping;
+
+baseTexture.wrapT = THREE.RepeatWrapping;
+ambientTexture.wrapT = THREE.RepeatWrapping;
+heightTexture.wrapT = THREE.RepeatWrapping;
+normalTexture.wrapT = THREE.RepeatWrapping;
+roughnessTexture.wrapT = THREE.RepeatWrapping;
+
 const pyramid = new THREE.Mesh(geometryPyramid, materialPyramid);
+pyramid.geometry.setAttribute(
+    'uv2',
+    new THREE.Float32BufferAttribute(pyramid.geometry.attributes.uv.array, 2),
+);
 pyramid.position.y = 3.8;
 pyramid.rotation.y = Math.PI;
 scene.add(pyramid);
 
 //create door to the piramid
 
-const geometryDoor = new THREE.PlaneGeometry(1, 1.8);
+const geometryDoor = new THREE.PlaneGeometry(1.2, 1.8);
 const materialDoor = new THREE.MeshBasicMaterial({
-    color: '#1353a5',
+    map: doorBaseTexture,
+    aoMap: doorAmbientTexture,
+    transparent: true,
+    normalMap: doorNormalTexture,
+    roughnessMap: doorRoughnessTexture,
     side: THREE.DoubleSide,
+    alphaMap: doorAlphaTexture,
+    displacementMap: doorHeightTexture,
+    displacementScale: 0.1,
+    metalnessMap: doorMentalnessTexture,
 });
 const door = new THREE.Mesh(geometryDoor, materialDoor);
+door.geometry.setAttribute(
+    'uv2',
+    new THREE.Float32BufferAttribute(door.geometry.attributes.uv.array, 2),
+);
 door.position.z = 2.08;
 door.position.y = 2.2;
 door.rotation.x = -0.45;
